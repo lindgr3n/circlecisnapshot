6534fb6aad86d615f8006176615e0e1f79066d8e
Johan Lindgren <johan@lindgr3n.com>
2017-07-09 14:14:46 +0200

    Initial commit

---
+++ src/Test/Test.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Test from './Test'
+import {shallow} from 'enzyme'
+
+describe('<Test>', () => {
+    it('Should render', () => {
+        const root = document.createElement('div')
+        ReactDOM.render(<Test />, root)
+    })
+
+    it('Should render with snapshot', () => {
+        const output = shallow(<Test />)
+        expect(Test).toMatchSnapshot()
+    })
+})
